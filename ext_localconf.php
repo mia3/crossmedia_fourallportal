63959a76a92cf8cebbe8d6f97237890bf0802861
Claus Due <claus@namelesscoder.net>
2017-09-29 12:17:49 +0200

    Remove FAL driver

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,14 +5,6 @@ if (!defined('TYPO3_MODE')) {
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\Crossmedia\Fourallportal\TypeConverter\FileReferenceTypeConverter::class);
 
-$registerDriver = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Resource\Driver\DriverRegistry::class);
-$registerDriver->registerDriverClass(
-    \Crossmedia\Fourallportal\Driver\MamDriver::class,
-    '4AllPortal',
-    '4AllPortal filesystem',
-    'FILE:EXT:fourallportal/Configuration/FlexForm/MamDriver.xml'
-);
-
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Crossmedia\Fourallportal\Task\EventHandler::class] = array(
     'extension' => $_EXTKEY,
     'title' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_scheduler.xlf:eventHandler.name',
