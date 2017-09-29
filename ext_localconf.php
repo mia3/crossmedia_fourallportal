43f8c12e459ae061422a0e327a1bcce778fc772a
Claus Due <claus@namelesscoder.net>
2017-09-29 12:41:40 +0200

    Remove old scheduler tasks
    Replaced by Extbase CommandController, see README.md

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,21 +4,6 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\Crossmedia\Fourallportal\TypeConverter\FileReferenceTypeConverter::class);
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Crossmedia\Fourallportal\Task\EventHandler::class] = array(
-    'extension' => $_EXTKEY,
-    'title' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_scheduler.xlf:eventHandler.name',
-    'description' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_scheduler.xlf:eventHandler.description',
-);
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Crossmedia\Fourallportal\Task\EventQueueHandler::class] = array(
-    'extension' => $_EXTKEY,
-    'title' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_scheduler.xlf:eventQueueHandler.name',
-    'description' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_scheduler.xlf:eventQueueHandler.description',
-    'additionalFields' => \Crossmedia\Fourallportal\Task\EventQueueHandlerFieldProvider::class,
-);
+\Crossmedia\Fourallportal\Mapping\MappingRegister::registerMapping(\Crossmedia\Fourallportal\Mapping\FalMapping::class);
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][$_EXTKEY] = \Crossmedia\Fourallportal\Command\FourallportalCommandController::class;
-
-
-\Crossmedia\Fourallportal\Mapping\MappingRegister::registerMapping(\Crossmedia\Fourallportal\Mapping\FalMapping::class);
