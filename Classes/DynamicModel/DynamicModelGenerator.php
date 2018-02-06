3294dd964c85f6a3ec34669ae542f91bd76543a0
Claus Due <claus@namelesscoder.net>
2018-02-06 11:44:53 +0100

    Add CELong to list of numeric values

--- Classes/DynamicModel/DynamicModelGenerator.php
+++ Classes/DynamicModel/DynamicModelGenerator.php
@@ -562,6 +562,7 @@ TEMPLATE;
                 break;
             case 'CETimestamp':
             case 'CEInteger':
+            case 'CELong':
             case 'MAMNumber':
             case 'XMPNumber':
                 $dataType = 'int';
