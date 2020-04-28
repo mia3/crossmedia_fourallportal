6b2c9026ddddc2cee0c0e3bb8558089a6cc9268c
Claus Due <claus@namelesscoder.net>
2020-04-28 16:07:00 +0200

    [TASK] Improve file reference data mapper
    Clean up the mapping logic to avoid a couple of redundant
    SQL requests.

--- Classes/TypeConverter/FileReferenceTypeConverter.php
+++ Classes/TypeConverter/FileReferenceTypeConverter.php
@@ -64,7 +64,6 @@ class FileReferenceTypeConverter extends AbstractUuidAwareObjectTypeConverter im
     public function convertFrom($source, $targetType, array $convertedChildProperties = [], PropertyMappingConfigurationInterface $configuration = null)
     {
         $dataMap = $this->objectManager->get(DataMapFactory::class)->buildDataMap(get_class($this->parentObject));
-        $resourceFactory = ResourceFactory::getInstance();
 
         $systemLanguageUid = (int)$this->parentObject->_getProperty('_languageUid');
         $fieldName = $dataMap->getColumnMap($this->propertyName)->getColumnName(); // GeneralUtility::camelCaseToLowerCaseUnderscored($this->propertyName);
@@ -76,9 +75,9 @@ class FileReferenceTypeConverter extends AbstractUuidAwareObjectTypeConverter im
         // exact same relation we were asked to convert - and we return the current property value.
         $queryBuilder = (new ConnectionPool())->getConnectionForTable('sys_file')->createQueryBuilder();
         $queryBuilder->getRestrictions()->removeAll()->add(GeneralUtility::makeInstance(DeletedRestriction::class));
-        $query = $queryBuilder->select('r.*')->from('sys_file', 'f')->from('sys_file_reference', 'r')->where(
+        $query = $queryBuilder->select('r.*')->from('sys_file', 'f')->join('f', 'sys_file_reference', 'r', 'r.uid_local = f.uid')->where(
             sprintf(
-                'r.uid_local = f.uid AND f.remote_id = \'%s\' AND r.tablenames = \'%s\' AND r.fieldname = \'%s\' AND r.uid_foreign = %d AND r.sys_language_uid = %d',
+                'f.remote_id = \'%s\' AND r.tablenames = \'%s\' AND r.fieldname = \'%s\' AND r.uid_foreign = %d AND r.sys_language_uid = %d',
                 $source,
                 $dataMap->getTableName(),
                 $fieldName,
@@ -89,87 +88,44 @@ class FileReferenceTypeConverter extends AbstractUuidAwareObjectTypeConverter im
         $references = $query->execute()->fetchAll();
         if (isset($references[0]['uid'])) {
             return $this->fetchObjectFromPersistence((int) $references[0]['uid'], $targetType);
-        } else {
-
-            // Lookup no. 2: try to find a sys_file with remote ID=$source and use it as target for a new
-            // file relation. If the original file cannot be found this way the relation is considered
-            // invalid or impossible to resolve - and an exception is thrown, causing the importing to be
-            // resumed on next run which should then have imported the target file so we can point to it.
-            $queryBuilder = (new ConnectionPool())->getConnectionForTable('sys_file')->createQueryBuilder();
-            $original = $queryBuilder->select('f.uid')->from('sys_file', 'f')
-                ->where($queryBuilder->expr()->eq('f.remote_id', $queryBuilder->quote($source)))
-                ->setMaxResults(1)
-                ->execute()
-                ->fetchAll();
-            if (!isset($original[0]['uid'])) {
-                $parentObjectId = method_exists($this->parentObject, 'getRemoteId') ? $this->parentObject->getRemoteId() : $this->parentObject->getUid();
-                throw new DeferralException(
-                    'Unable to map ' . $this->propertyName . ' on ' . get_class($this->parentObject) . ':' . $parentObjectId .
-                    ' - Asset ' . $source . ' does not appear to exist (yet).',
-                    1527167261
-                );
-            }
-
-            // File reference object needs to be created with the exact composition of this array. Not
-            // passing either one of these parameters causes an invalid file reference to be written.
-            $referenceProperties = [
-                //'pid' => $this->parentObject->getPid(),
-                'tablenames' => $dataMap->getTableName(),
-                'table_local' => 'sys_file',
-                'fieldname' => $fieldName,
-                'uid_local' => $original[0]['uid'],
-                'uid_foreign' => $this->parentObject->getUid(),
-                'sys_language_uid' => $systemLanguageUid,
-            ];
-
         }
 
-        /*
-        if ($systemLanguageUid > 0) {
-            // Select the original language version of this particular file reference so the UID can
-            // be used as t3_origuid and l10n_parent value in the localized reference's record.
-            // Note that it is possible that there is no default language record (emulated free mode
-            // in IRRE) so we only create the reference as a localized version IF there is a default.
-            $queryBuilder = (new ConnectionPool())->getConnectionForTable('sys_file')->createQueryBuilder();
-            $query = $queryBuilder->select('r.uid')->from('sys_file', 'f')->from('sys_file_reference', 'r')->where(
-                sprintf(
-                    'r.uid_local = f.uid AND f.remote_id = \'%s\' AND r.tablenames = \'%s\' AND r.table_local = \'sys_file\' AND r.fieldname = \'%s\' AND r.uid_foreign = %d AND r.sys_language_uid = ' . $this->parentObject->_getProperty('_languageUid'),
-                    $source,
-                    $dataMap->getTableName(),
-                    $fieldName,
-                    $this->parentObject->getUid()
-                )
-            )->setMaxResults(1);
-            $originalLanguageReferenceRecord = $query->execute()->fetch();
-            if ($originalLanguageReferenceRecord) {
-                $referenceProperties['l10n_parent'] = $originalLanguageReferenceRecord['uid'];
-                $referenceProperties['t3_origuid'] = $originalLanguageReferenceRecord['uid'];
-                $referenceProperties['uid_foreign'] = $this->parentObject->_getProperty('_localizedUid');
-            }
-        }
-        */
-
-        $reference = $resourceFactory->createFileReferenceObject($referenceProperties);
-
-        if (!isset($reference)) {
+        // Lookup no. 2: try to find a sys_file with remote ID=$source and use it as target for a new
+        // file relation. If the original file cannot be found this way the relation is considered
+        // invalid or impossible to resolve - and an exception is thrown, causing the importing to be
+        // resumed on next run which should then have imported the target file so we can point to it.
+        $queryBuilder = (new ConnectionPool())->getConnectionForTable('sys_file')->createQueryBuilder();
+        $original = $queryBuilder->select('f.uid')->from('sys_file', 'f')
+            ->where($queryBuilder->expr()->eq('f.remote_id', $queryBuilder->quote($source)))
+            ->setMaxResults(1)
+            ->execute()
+            ->fetchAll();
+        if (!isset($original[0]['uid'])) {
+            $parentObjectId = method_exists($this->parentObject, 'getRemoteId') ? $this->parentObject->getRemoteId() : $this->parentObject->getUid();
             throw new DeferralException(
-                'Unable to map ' . $this->propertyName . ' on ' . get_class($this->parentObject) . ':' . $this->parentObject->getUid() .
+                'Unable to map ' . $this->propertyName . ' on ' . get_class($this->parentObject) . ':' . $parentObjectId .
                 ' - Asset ' . $source . ' does not appear to exist (yet).',
-                1527167262
+                1527167261
             );
         }
 
-        // New Extbase model FileReference instance is fitted with target and returned. The resulting
-        // object persists correctly because the $reference above has had all properties manually set.
-        $model = new FileReference();
-        $model->_memorizeCleanState();
-        $model->setOriginalResource($reference);
-        $model->_setProperty('_languageUid', $systemLanguageUid);
-        if ($systemLanguageUid > 0 && ($originalLanguageReferenceRecord['uid'] ?? false)) {
-            $model->_setProperty('_localizedUid', $reference->getUid());
-        }
-        //$model->setPid($this->parentObject->getPid());
-        return $model;
+        // File reference object needs to be created with the exact composition of this array. Not
+        // passing either one of these parameters causes an invalid file reference to be written.
+        $referenceProperties = [
+            //'pid' => $this->parentObject->getPid(),
+            'tablenames' => $dataMap->getTableName(),
+            'table_local' => 'sys_file',
+            'fieldname' => $fieldName,
+            'uid_local' => $original[0]['uid'],
+            'uid_foreign' => $this->parentObject->getUid(),
+            'sys_language_uid' => $systemLanguageUid,
+        ];
+
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('sys_file_reference');
+        $connection->insert('sys_file_reference', $referenceProperties);
+        $referenceProperties['uid'] = $connection->lastInsertId('sys_file_reference');
+        $object = $this->fetchObjectFromPersistence((int) $referenceProperties['uid'], $targetType);
+        return $object;
     }
 
     /**
